f62f92fd6669b511fb6f45d627ad5dfff0747b31
Marcos A Rosas <contactme@marcosarosas.dev>
2023-05-13 21:31:05 -0500

    updated table form styles

--- front-end/src/tables/TableForm.jsx
+++ front-end/src/tables/TableForm.jsx
@@ -33,7 +33,7 @@ function TableForm({ submitForm }) {
 
   function validateTable() {
     const capacity = Number(form.capacity);
-    
+
     if (capacity < 1 || !Number.isInteger(capacity)) {
       return 'Capacity must be a at least 1 person.';
     }
@@ -45,55 +45,55 @@ function TableForm({ submitForm }) {
     return null;
   }
 
-
   return (
     <main className='min-vh-md-100'>
-      <div>
-      <ErrorAlert error={errors} />
-      <form onSubmit={handleSubmit} className='row g-3'>
-        <div className='form-group'>
-          <div className='form-floating col-6 mb-3'>
-            <input
-              id='table_name'
-              name='table_name'
-              type='text'
-              className='form-control mt-5'
-              placeholder='Table Name'
-              onChange={handleChange}
-              value={form.table_name}
-              required
-            />
-            <label htmlFor='table_name'>Table Name</label>
-          </div>
-
-          <div className='form-floating col-6'>
-            <input
-              id='capacity'
-              name='capacity'
-              type='number'
-              className='form-control'
-              placeholder='Capacity'
-              onChange={handleChange}
-              value={form.capacity}
-              required
-            />
-            <label htmlFor='capacity'>Capacity</label>
-          </div>
-
-          <div className='form-group mt-3'>
-            <button
-              type='button'
-              className='btn btn-secondary me-2'
-              onClick={() => history.goBack()}
-            >
-              Cancel
-            </button>
-            <button type='submit' className='btn btn-primary m-1'>
-              Submit
-            </button>
+      <div className='ms-3'>
+        <h4 className='mt-5'>New Table</h4>
+        <ErrorAlert error={errors} />
+        <form onSubmit={handleSubmit} className='row g-3'>
+          <div className='form-group'>
+            <div className='form-floating col-6 mb-3'>
+              <input
+                id='table_name'
+                name='table_name'
+                type='text'
+                className='form-control mt-3'
+                placeholder='Table Name'
+                onChange={handleChange}
+                value={form.table_name}
+                required
+              />
+              <label htmlFor='table_name'>Table Name</label>
             </div>
-        </div>
-      </form>
+
+            <div className='form-floating col-6'>
+              <input
+                id='capacity'
+                name='capacity'
+                type='number'
+                className='form-control'
+                placeholder='Capacity'
+                onChange={handleChange}
+                value={form.capacity}
+                required
+              />
+              <label htmlFor='capacity'>Capacity</label>
+            </div>
+
+            <div className='form-group mt-3'>
+              <button
+                type='button'
+                className='btn btn-secondary me-2'
+                onClick={() => history.goBack()}
+              >
+                Cancel
+              </button>
+              <button type='submit' className='btn btn-primary m-1'>
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </main>
   );
